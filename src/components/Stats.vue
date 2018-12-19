4a0bb866fc73b6bc4794fe57da7427f25f43c0ad
Ekwejunor-Etchie <aekwej2@uic.edu>
2018-12-19 15:00:23 -0600

    vuex practice, unlisted app

---
+++ src/components/Stats.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="stats">
+    <h1>Another Component</h1>
+    <p>There are currently {{linkCount}} Savage links</p>
+    <button @click="rmAll">Remove All</button>
+    <h2>{{msg}}</h2>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapMutations, mapActions } from 'vuex'
+
+export default {
+  name: 'Stats',
+  data(){
+    return {
+      msg: ''
+    }
+  },
+  computed: {
+    ...mapGetters(['linkCount'])
+  },
+  methods: {
+    ...mapMutations(['Remove_All']),
+    ...mapActions(['removeAll']),
+    rmAll() {
+      this.removeAll().then(() => {
+        this.msg = 'Zeus Has Been Satisfied'
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+button {
+    padding: 10px;
+    margin-top: 30px;
+    width: 100%;
+    background: none;
+    border: 1px solid lightgray;
+    outline: 0;
+    cursor: pointer;
+}
+</style>
--- src/store.js
+++ src/store.js
@@ -5,12 +5,40 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+  	title: 'Supreme Title',
+  	links: [
+  		'google.com',
+  		'youtube.com',
+  		'uic.edu'
+  	]
+  },
+  getters: {
+    linkCount(state) {
+      return state.links.length
+    }
   },
   mutations: {
-
+    ADD_LINK(state, link) {
+      return state.links.push(link)
+    },
+    REMOVE_LINK(state, link) {
+      return state.links.splice(link, 1)
+    },
+    REMOVE_All(state) {
+      state.links = []
+    }
   },
   actions: {
-
+    removeLink(context, link) {
+      context.commit('REMOVE_LINK', link)
+    },
+    removeAll({commit}) {
+      return new Promise((res, rej) => {
+        setTimeout(() => {
+          commit('REMOVE_All')
+          res()
+        }, 1500);
+      })
+    }
   }
 })
